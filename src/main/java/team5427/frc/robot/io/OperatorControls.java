858d64a077213e86a7b4695d56bd8fdb0397fdc8
AaronZhenger <alock8mini@gmail.com>
2026-07-24 18:40:07 -0500

    Commands

--- src/main/java/team5427/frc/robot/io/OperatorControls.java
+++ src/main/java/team5427/frc/robot/io/OperatorControls.java
@@ -3,8 +3,12 @@ package team5427.frc.robot.io;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import team5427.frc.robot.Constants.DriverConstants;
 import team5427.frc.robot.Superstructure;
+import team5427.frc.robot.Superstructure.DiffyStates;
 import team5427.frc.robot.Superstructure.ElevatorStates;
+import team5427.frc.robot.commands.diffy.FlickWrist;
+import team5427.frc.robot.commands.diffy.RotateWrist;
 import team5427.frc.robot.commands.elevator.MoveElevatorToPosition;
+import team5427.frc.robot.subsystems.diffy.DiffyConstants;
 import team5427.frc.robot.subsystems.elevator.ElevatorConstants;
 
 public class OperatorControls {
@@ -49,11 +53,25 @@ public class OperatorControls {
     //               IntakeSubsystem.getInstance().disablePivotMotor(false);
     //               IntakeSubsystem.getInstance().disableRollerMotor(false);
     //             }));
-    joy.a().onTrue(Superstructure.setElevatorStateCommand(ElevatorStates.SETPOINT1));
+    joy.a()
+        .onTrue(Superstructure.setElevatorStateCommand(ElevatorStates.SETPOINT1))
+        .onTrue(Superstructure.setDiffyStateCommand(DiffyStates.SCORING));
 
-    joy.b().onTrue(Superstructure.setElevatorStateCommand(ElevatorStates.SETPOINT2));
+    joy.x()
+        .onTrue(Superstructure.setElevatorStateCommand(ElevatorStates.SETPOINT2))
+        .onTrue(Superstructure.setDiffyStateCommand(DiffyStates.SCORING));
 
-    joy.povDown().onTrue(Superstructure.setElevatorStateCommand(ElevatorStates.STOWED));
+    joy.povDown()
+        .onTrue(Superstructure.setElevatorStateCommand(ElevatorStates.STOWED))
+        .onTrue(Superstructure.setDiffyStateCommand(DiffyStates.STOWED));
+
+    joy.leftTrigger()
+        .onTrue(Superstructure.setDiffyStateCommand(DiffyStates.STATION_INTAKING))
+        .onFalse(Superstructure.setDiffyStateCommand(DiffyStates.STOWED));
+
+    joy.leftBumper()
+        .onTrue(Superstructure.setDiffyStateCommand(DiffyStates.GROUND_INTAKING))
+        .onFalse(Superstructure.setDiffyStateCommand(DiffyStates.STOWED));
 
     Superstructure.elevatorStateIs(ElevatorStates.SETPOINT1)
         .whileTrue(new MoveElevatorToPosition(ElevatorConstants.kElevatorSetpoint1));
@@ -61,5 +79,17 @@ public class OperatorControls {
         .whileTrue(new MoveElevatorToPosition(ElevatorConstants.kElevatorSetpoint2));
     Superstructure.elevatorStateIs(ElevatorStates.STOWED)
         .whileTrue(new MoveElevatorToPosition(ElevatorConstants.kElevatorStowPosition));
+    Superstructure.diffyStateIs(DiffyStates.STOWED)
+        .whileTrue(new RotateWrist(DiffyConstants.kAverageStowPosition))
+        .whileTrue(new FlickWrist(DiffyConstants.kDifferenceStandardPosition));
+    Superstructure.diffyStateIs(DiffyStates.STATION_INTAKING)
+        .whileTrue(new RotateWrist(DiffyConstants.kAverageStationPosition))
+        .whileTrue(new FlickWrist(DiffyConstants.kDifferenceHorizontalPosition));
+    Superstructure.diffyStateIs(DiffyStates.GROUND_INTAKING)
+        .whileTrue(new RotateWrist(DiffyConstants.kAverageGroundPosition))
+        .whileTrue(new FlickWrist(DiffyConstants.kDifferenceHorizontalPosition));
+    Superstructure.diffyStateIs(DiffyStates.SCORING)
+        .whileTrue(new RotateWrist(DiffyConstants.kAverageScoringPosition))
+        .whileTrue(new FlickWrist(DiffyConstants.kDifferenceStandardPosition));
   }
 }
